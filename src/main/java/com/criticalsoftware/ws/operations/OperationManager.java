9ec7cfe8550fd0541bd752165c62e17da0956a1f
jmssantos <joao.s.santos@criticalsoftware.com>
2018-08-10 18:04:29 +0100

    Created RequestOperationModel and added Jackson FW dependency

--- src/main/java/com/criticalsoftware/ws/operations/OperationManager.java
+++ src/main/java/com/criticalsoftware/ws/operations/OperationManager.java
@@ -1,24 +1,32 @@
 package com.criticalsoftware.ws.operations;
 
-public class OperationManager {
+public final class OperationManager {
 
-	public double add(double value1, double value2) {
+	private OperationManager() {
+		
+	}
+	
+	private static boolean checkValidOperation(String operation) {
+		return true;
+	}
+	
+	private static double add(double value1, double value2) {
 		return (value1 + value2);
 	}
  
-	public double subtract(double value1, double value2) {
+	private static double subtract(double value1, double value2) {
 		return (value1 - value2);
 	}
 	
-	public double multiply(double value1, double value2) {
+	private static double multiply(double value1, double value2) {
 		return (value1 * value2);
 	}
 	
-	public double divide(double value1, double value2) {
+	private static double divide(double value1, double value2) {
 		return (value1 / value2);
 	}
 	
-	public double average(double value1, double value2) {
+	private static double average(double value1, double value2) {
 		return ((value1 + value2) / 2);
 	}
 }
--- src/main/java/com/criticalsoftware/ws/service/OperationsServlet.java
+++ src/main/java/com/criticalsoftware/ws/service/OperationsServlet.java
@@ -1,29 +1,50 @@
 package com.criticalsoftware.ws.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.criticalsoftware.ws.models.OperationRequestModel;
+import com.criticalsoftware.ws.operations.OperationManager;;
+
 public class OperationsServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-
-//	@Override
-//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		resp.getWriter().println("I made it!");			
-//	}
 	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	@Override	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
-			String jsonData = req.toString();
 			
-			System.out.println(jsonData);
-			
+			//Reads the JSON request
+			StringBuilder sb = new StringBuilder();
+		    BufferedReader reader = request.getReader();
+		    	
+		    try {
+		        String line;
+		        while ((line = reader.readLine()) != null) {
+		            sb.append(line).append('\n');
+		        }
+		    } finally {
+		        reader.close();
+		    }
+		    		    
+		    String jsonData = sb.toString();					    		   
+		    ObjectMapper objectMapper = new ObjectMapper();
+		    
+		    //Serializes the received JSON to a new OperationRequestModel
+		    OperationRequestModel operationModel = objectMapper.readValue(jsonData, OperationRequestModel.class);		      
+		    			
 		} catch (Exception e) {
-			resp.getWriter().println("Invalid JSON Object.");
+			response.getWriter().println("Invalid JSON Object.");
 		}		
 	}
 	
