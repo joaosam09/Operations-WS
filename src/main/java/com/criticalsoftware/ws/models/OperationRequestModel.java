96ad8da6d7014479b29966b1eeaabb8260153c16
jmssantos <joao.s.santos@criticalsoftware.com>
2018-08-10 16:26:11 +0100

    Initial project model

---
+++ src/main/java/com/criticalsoftware/ws/models/OperationRequestModel.java
@@ -0,0 +1,31 @@
+package com.criticalsoftware.ws.models;
+
+public class OperationRequestModel {
+	private double value1;
+	private double value2;
+	private String operation;
+	
+	public double getValue1() {
+		return value1;
+	}
+	
+	public void setValue1(double value) {
+		this.value1 = value;
+	}
+	
+	public void setValue2(double value) {
+		this.value2 = value;
+	}
+	
+	public double getValue2() {
+		return value2;
+	}
+	
+	public String getOperation() {
+		return operation;
+	}
+	
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
+}
--- src/main/java/com/criticalsoftware/ws/service/OperationsServlet.java
+++ src/main/java/com/criticalsoftware/ws/service/OperationsServlet.java
@@ -6,18 +6,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.criticalsoftware.ws.models.OperationRequestModel;
+
 public class OperationsServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().println("I made it!");			
-	}
+//	@Override
+//	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		resp.getWriter().println("I made it!");			
+//	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				
+		try {
+			String jsonData = req.toString();
+			
+			System.out.println(jsonData);
+			
+		} catch (Exception e) {
+			resp.getWriter().println("Invalid JSON Object.");
+		}
 	}
 	
 	@Override
